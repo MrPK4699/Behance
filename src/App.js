9da4800dabfed87da4386f7ed93cbe55e7ecac5c
Pankaj Khatri <4699pankajkhatri@gmail.com>
2024-01-04 14:47:56 +0530

    InitialCommit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Project Behance</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
